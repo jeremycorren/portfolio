42291ffe10a4e1f3340b17e7f9d3057df5809b80
Jeremy Corren <jeremycorren@Jeremys-MacBook-Pro.local>
2018-02-10 22:48:10 -0500

    Refactor server port

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'client/public')));
 app.use('/api', require('./routes/index'));
 
-app.get('/', (req, res) => {
-	res.redirect('/api');
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 app.use((req, res, next) => {
